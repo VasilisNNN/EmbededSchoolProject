48d1e8771b003724e64b3d9d2d4641cc6a0b12c7
Mykyta <mykyta@marginalact.com>
2026-08-12 12:04:23 +0300

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,10 @@
 
 #define LED_OUT_RED 15
 #define BUTTON_BOOT 0
-
 #define BUTTON_IN 16
 
-float ledSpeed = 0;
-float prevledSpeed = -1;
+u8_t blinkSpeed = 0;
+bool legTurnedOn = false;
 
 void setup()
 {
@@ -17,38 +16,40 @@ void setup()
     pinMode(LED_OUT_RED, OUTPUT);
     digitalWrite(LED_OUT_RED, LOW);
 
-    pinMode(BUTTON_IN, INPUT_PULLDOWN);
+    pinMode(BUTTON_IN, INPUT_PULLUP);
 }
 
 void loop()
 {
+    speedManager();
+    ledManager();
+}
 
+void speedManager()
+{
     int input = digitalRead(BUTTON_IN);
     int inputBoot = digitalRead(BUTTON_BOOT);
-    static bool legTurnedOn = false;
 
     if (input == 1)
     {
-        ledSpeed = 1;
+        blinkSpeed = 1;
         delay(5);
     }
     if (inputBoot == 1)
     {
-        ledSpeed = 2;
+        blinkSpeed = 2;
         delay(5);
     }
+}
 
-    
-    if (ledSpeed == 0)
+void ledManager()
+{
+    if (blinkSpeed == 0)
     {
 
         digitalWrite(LED_OUT_RED, LOW);
+        Serial.println("LED OFF");
 
-        if (prevledSpeed != ledSpeed)
-        {
-            Serial.println("LED OFF");
-            prevledSpeed = ledSpeed;
-        }
         return;
     }
 
@@ -65,5 +66,5 @@ void loop()
         digitalWrite(LED_OUT_RED, LOW);
     }
 
-    delay(100);
+    delay(400 * blinkSpeed);
 }
