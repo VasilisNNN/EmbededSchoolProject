f0e4c147bc1d70a04c83fe53645c5274cb856cd0
Mykyta <mykyta@marginalact.com>
2026-08-12 11:52:11 +0300

    2 modes

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,69 @@
 #include <Arduino.h>
 
 #define LED_OUT_RED 15
+#define BUTTON_BOOT 0
+
 #define BUTTON_IN 16
 
-bool ledState = false;
-bool prevledState = true;
+float ledSpeed = 0;
+float prevledSpeed = -1;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     delay(1000);
 
+    pinMode(BUTTON_BOOT, INPUT_PULLDOWN);
     pinMode(LED_OUT_RED, OUTPUT);
     digitalWrite(LED_OUT_RED, LOW);
 
     pinMode(BUTTON_IN, INPUT_PULLDOWN);
-  
 }
 
-void loop() {
+void loop()
+{
 
     int input = digitalRead(BUTTON_IN);
-     
-      if (input == 1)ledState = true;
-      else ledState = false;
+    int inputBoot = digitalRead(BUTTON_BOOT);
+    static bool legTurnedOn = false;
 
+    if (input == 1)
+    {
+        ledSpeed = 1;
+        delay(5);
+    }
+    if (inputBoot == 1)
+    {
+        ledSpeed = 2;
+        delay(5);
+    }
 
-    if (ledState) {
-        digitalWrite(LED_OUT_RED, HIGH);
+    
+    if (ledSpeed == 0)
+    {
 
-        if(prevledState!=ledState)
-        {
-        Serial.println("LED ON");
-        prevledState = ledState;
-        }
-
-
-    } else {
         digitalWrite(LED_OUT_RED, LOW);
 
-      
-        if(prevledState!=ledState)
+        if (prevledSpeed != ledSpeed)
         {
-        Serial.println("LED OFF");
-        prevledState = ledState;
+            Serial.println("LED OFF");
+            prevledSpeed = ledSpeed;
         }
+        return;
     }
-    
 
- 
+    legTurnedOn = !legTurnedOn;
+
+    if (legTurnedOn)
+    {
+        Serial.println("LED ON");
+        digitalWrite(LED_OUT_RED, HIGH);
+    }
+    else
+    {
+        Serial.println("LED OFF");
+        digitalWrite(LED_OUT_RED, LOW);
+    }
+
+    delay(100);
 }
