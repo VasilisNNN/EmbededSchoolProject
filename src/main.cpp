5e62669efc82eb7ea137b1d819452c530435b5db
Mykyta <mykyta@marginalact.com>
2026-08-15 13:38:07 +0300

    start

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,35 @@
 #include <Arduino.h>
 
-#define LED_RED 15
-#define LED_BLUE 16
+
+#define PIN_IN 16
+#define ADCmax 4095
 
 void setup()
 {
-    pinMode(LED_RED, OUTPUT);
-    pinMode(LED_BLUE, OUTPUT);
+    Serial.begin(115200);
 
-    digitalWrite(LED_RED, LOW);
-    digitalWrite(LED_BLUE, LOW);
+    pinMode(PIN_IN, INPUT);
+
+
 }
 
 void loop()
 {
-  
-    digitalWrite(LED_RED, HIGH);
-    digitalWrite(LED_BLUE, LOW);
-    delay(500);
+  int ADCValue = digitalRead(PIN_IN);
+ int AnalogValue = analogRead(PIN_IN);
+  int AnalogVoltage = analogReadMilliVolts(PIN_IN);
 
-    digitalWrite(LED_RED, LOW);
-    digitalWrite(LED_BLUE, HIGH);
-    delay(500);
+ const float URef = 3100;
+
+ float Voltage = ((float)AnalogValue/(float)4095) * URef;
+
+ float error = abs(Voltage - AnalogVoltage) / AnalogVoltage * 100.0;
+
+ Serial.printf("digitalRead: %d \n", ADCValue);
+ Serial.printf("AnalogValue: %d mV\n", AnalogValue);
+ Serial.printf("Voltage: %.02f V\n", Voltage);
+ Serial.printf("AnalogValueM: %.02f mV\n", AnalogVoltage);
+ Serial.printf("error: %.02f mV\n", error);
+
+    delay(100);
 }
