ab0379db43c7e7c434ed66f38613de1349800ac6
Mykyta <mykyta@marginalact.com>
2026-08-17 20:27:46 +0300

    fixing onoff switch

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,62 @@
 #include <Arduino.h>
 
-
 #define PIN_IN 16
 #define PIN_OUT 17
 
+#define ADCres 4095
+bool turnedOn = false;
+float switchTimer = 0;
 
 void setup()
 {
     Serial.begin(115200);
 
-    pinMode(PIN_IN, INPUT_PULLDOWN);
+    pinMode(PIN_IN, INPUT_PULLUP);
     pinMode(PIN_OUT, OUTPUT);
+}
+void SwitchOnOff(float voltage)
+{
+  
+    if (switchTimer > millis())
+        return;
 
+    if (voltage < 1.5f)
+    {
+        turnedOn = true;
+
+    }
+    else if (voltage > 10.5f)
+    {
+        turnedOn = false;
+
+    }
+
+    switchTimer = millis() + 2;
 }
 
 void loop()
 {
-  int ADCValue = digitalRead(PIN_IN);
- int AnalogValue = analogRead(PIN_IN);
-  int AnalogVoltage = analogReadMilliVolts(PIN_IN);
+    int ADCValue = digitalRead(PIN_IN);
+    int AnalogValue = analogRead(PIN_IN);
+    int AnalogVoltage = analogReadMilliVolts(PIN_IN);
 
- const float URef = 3100;
+    const float URef = 3100;
 
- float Voltage = ((float)AnalogValue/(float)4095) * URef;
+    float Voltage = ((float)AnalogValue / (float)ADCres) * URef;
 
- if(Voltage<2.4f)digitalWrite(PIN_OUT, 1);
+    SwitchOnOff(Voltage);
 
- Serial.printf("digitalRead: %d \n", ADCValue);
- Serial.printf("AnalogValue: %d mV\n", AnalogValue);
- Serial.printf("Voltage: %.02f V\n", Voltage);
- Serial.printf("AnalogValueM: %.02f mV\n", AnalogVoltage);
- 
+    if (turnedOn)
+        digitalWrite(PIN_OUT, HIGH);
+    else
+        digitalWrite(PIN_OUT, LOW);
+
+    Serial.printf("digitalRead: %d \n", ADCValue);
+    Serial.printf("AnalogValue: %d mV\n", AnalogValue);
+    Serial.printf("Voltage: %.02f mV\n", Voltage);
+    Serial.printf("AnalogValueM: %.02f mV\n", AnalogVoltage);
+
     delay(100);
 }
+
+
