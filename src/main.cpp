3a352c2b4b41af784703dca4253c0ce3234a914e
Mykyta <mykyta@marginalact.com>
2026-08-17 22:52:05 +0300

    start1

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,35 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 13
+#define LED_PIN     48  // YD-ESP32-S3 onboard WS2812 (IO48)
+#define NUMPIXELS   1   // One RGB LED only
 
-
+Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
     Serial.begin(115200);
-    while (!Serial) {
+    pixels.begin();
+    pixels.setBrightness(100); // Brightness (0~255)
+    Serial.println("Rainbow demo start with brightness 100");
+}
+
+uint32_t Wheel(byte pos) {
+    pos = 255 - pos;
+    if(pos < 85) {
+        return pixels.Color(255 - pos * 3, 0, pos * 3);
+    } else if(pos < 170) {
+        pos -= 85;
+        return pixels.Color(0, pos * 3, 255 - pos * 3);
+    } else {
+        pos -= 170;
+        return pixels.Color(pos * 3, 255 - pos * 3, 0);
     }
-    Serial.println("UART ready!\r\n");
 }
 
 void loop() {
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-    delay(500);
-    Serial.println("LED blinked!");
+    for(int i = 0; i < 256; i++) {
+        pixels.setPixelColor(0, Wheel(i));
+        pixels.show();
+        delay(20);
+    }
 }
-
-
