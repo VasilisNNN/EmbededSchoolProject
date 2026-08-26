bc0e3c0236b38e5800c64907f7aa4d7e2576bddb
Mykyta <mykyta@marginalact.com>
2026-08-26 20:59:45 +0300

    button lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,47 @@
 #include <Arduino.h>
+#include "buttonFSM.h"
 
-#define PIN_IN 16
-#define PIN_OUT 17
+#define BUTTON_PIN 			15
+#define LED_PIN 			16
+#define DEBOUNCE_DELAY 		50
 
-#define ADCres 4095
-bool turnedOn = false;
-float switchTimer = 0;
+typedef struct{
+	int presseCount;
+	int releaseCount;
+} Counter_t;
 
-#define MinSwitchmV 80.0f
-#define MaxSwitchmV 120.0f
-#define TimderDelay 2.0f
+static Button_FSM_t buttonFsm;
+static Counter_t counter = {0, 0};
 
-void setup()
-{
-    Serial.begin(115200);
-
-    pinMode(PIN_IN, INPUT_PULLUP);
-    pinMode(PIN_OUT, OUTPUT);
-}
-void SwitchOnOff(float voltage)
-{
-  
-    if (switchTimer > millis())
-        return;
-
-    if (voltage < MinSwitchmV)
-    {
-        turnedOn = true;
-
-    }
-    else if (voltage > MaxSwitchmV)
-    {
-        turnedOn = false;
-
-    }
-
-    switchTimer = millis() + TimderDelay;
+void onButtonPress(void* arg) {
+	Counter_t* counter = (Counter_t*)arg;
+	counter->presseCount++;
+	Serial.print("Button pressed: ");
+	Serial.println(counter->presseCount);
+	digitalWrite(LED_PIN, HIGH);
 }
 
-void loop()
-{
-    int ADCValue = digitalRead(PIN_IN);
-    int AnalogValue = analogRead(PIN_IN);
-    int AnalogVoltage = analogReadMilliVolts(PIN_IN);
-
-    const float URef = 3100;
-
-    float Voltage = ((float)AnalogValue / (float)ADCres) * URef;
-
-    SwitchOnOff(Voltage);
-
-    if (turnedOn)
-        digitalWrite(PIN_OUT, HIGH);
-    else
-        digitalWrite(PIN_OUT, LOW);
-
-    Serial.printf("digitalRead: %d \n", ADCValue);
-    Serial.printf("AnalogValue: %d mV\n", AnalogValue);
-    Serial.printf("Voltage: %.02f mV\n", Voltage);
-    Serial.printf("AnalogValueM: %.02f mV\n", AnalogVoltage);
-
-    delay(100);
+void onButtonRelease(void* arg) {
+	Counter_t* counter = (Counter_t*)arg;
+	counter->releaseCount++;
+	Serial.print("Button released: ");
+	Serial.println(counter->releaseCount);
+	digitalWrite(LED_PIN, LOW);
 }
 
+void setup() {
+	Serial.begin(115200);
+	pinMode(BUTTON_PIN, INPUT_PULLUP);
+	pinMode(LED_PIN, OUTPUT);
+	digitalWrite(LED_PIN, LOW);
 
+	if (Button_FSM_Init(&buttonFsm, BUTTON_PIN, DEBOUNCE_DELAY, onButtonPress, onButtonRelease, &counter) != 0) {
+		Serial.println("Failed to initialize button FSM");
+	}
+}
+
+void loop() {
+	Button_FSM_Update(&buttonFsm);
+
+	delay(1);
+}
