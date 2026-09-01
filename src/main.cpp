0403fbcb25a82171e5ff7e380df856a2095f9e25
Mykyta <mykyta@marginalact.com>
2026-09-01 19:08:08 +0300

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,23 @@
-<<<<<<< HEAD
-#include <Arduino.h>
-
-#define PIN_IN 16
-#define PIN_OUT 17
-=======
 #include "Arduino.h"
 #include "Led.h"
 #include "Config.h"
->>>>>>> 522a0e0 (blink on timer)
 
-#define ADCres 4095
-bool turnedOn = false;
-float switchTimer = 0;
-
-#define MinSwitchmV 80.0f
-#define MaxSwitchmV 120.0f
-#define TimderDelay 2.0f
-
+Led led;
+float timer;
 void setup()
 {
     Serial.begin(115200);
 
-<<<<<<< HEAD
-    pinMode(PIN_IN, INPUT_PULLUP);
-    pinMode(PIN_OUT, OUTPUT);
-=======
+
     led.Init(Config::PIN_OUT);
   
 
->>>>>>> 522a0e0 (blink on timer)
 }
-void SwitchOnOff(float voltage)
-{
-  
-    if (switchTimer > millis())
-        return;
 
-    if (voltage < MinSwitchmV)
-    {
-        turnedOn = true;
-
-    }
-    else if (voltage > MaxSwitchmV)
-    {
-        turnedOn = false;
-
-    }
-
-    switchTimer = millis() + TimderDelay;
-}
 
 void loop()
 {
-    int ADCValue = digitalRead(PIN_IN);
-    int AnalogValue = analogRead(PIN_IN);
-    int AnalogVoltage = analogReadMilliVolts(PIN_IN);
-
-<<<<<<< HEAD
-    const float URef = 3100;
-
-    float Voltage = ((float)AnalogValue / (float)ADCres) * URef;
-
-    SwitchOnOff(Voltage);
-
-    if (turnedOn)
-        digitalWrite(PIN_OUT, HIGH);
-    else
-        digitalWrite(PIN_OUT, LOW);
-
-    Serial.printf("digitalRead: %d \n", ADCValue);
-    Serial.printf("AnalogValue: %d mV\n", AnalogValue);
-    Serial.printf("Voltage: %.02f mV\n", Voltage);
-    Serial.printf("AnalogValueM: %.02f mV\n", AnalogVoltage);
-
-    delay(100);
-=======
+ 
     if(timer > millis() && timer- Config::BLINK_TIME_MS < millis() )
 	{
 	    led.Set(LedState::Off);
@@ -87,7 +30,6 @@ void loop()
 	if(timer < millis())
     timer =  millis() + Config::BLINK_TIME_MS*2;
     
->>>>>>> 522a0e0 (blink on timer)
 }
 
 
