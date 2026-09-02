64c9366e65e7e9cc7a75d6900d367dd1ce511ada
Mykyta <mykyta@marginalact.com>
2026-09-02 20:51:24 +0300

    start

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,70 @@
-#include "Arduino.h"
-#include "main.h"
-#include "Config.h"
+#include <Arduino.h>
+#include <atomic>
+#include <OneButton.h>
+#include <esp_task_wdt.h>
 
-Led led_0;
-Led led_1;
-Led led_2;
+#define BTN_START_TIMER_IN 15
+#define TIMER_LED_OUT 16
+#define WDT_TIMEOUT_S 10
 
-void setup()
-{
-    Serial.begin(115200);
+std::atomic<uint32_t> isrCounter(0);
+volatile bool timerFired = false;
 
+hw_timer_t *timer = NULL;
+OneButton startButton(BTN_START_TIMER_IN, true, true);
+uint32_t timeDuration = 0;
 
-    led_0.Init(Config::PIN_OUT_0);
-    led_1.Init(Config::PIN_OUT_1);
-    led_2.Init(Config::PIN_OUT_2);
+// Функція переривання (ISR)
+void IRAM_ATTR onTimer() {
+	// Операції ++ та присвоєння для atomic є безпечними (атомарними)
+	isrCounter.fetch_add(1, std::memory_order_relaxed);
+	timerFired = true;
 }
 
-
-void loop()
-{
-
-  blink(led_0, Config::BLINK_TIME_MS_0);
-  blink(led_1, Config::BLINK_TIME_MS_1);
-  blink(led_2, Config::BLINK_TIME_MS_2);
-
+void startTimer() {
+	digitalWrite(TIMER_LED_OUT, HIGH);
+	// Старт таймера
+	timerWrite(timer, 0);
+	timerAlarmWrite(timer, 1000000, false);
+	timerAlarmEnable(timer);
+	// Запис поточного часу
+	timeDuration = millis();
 }
 
-void blink(Led& currectLed, float delay)
-{
-    if(currectLed.timer > millis() && currectLed.timer - delay < millis() )
-	{
-	    currectLed.Set(LedState::Off);
-		return;
+void setup() {
+	Serial.begin(115200);
+	//enableLoopWDT();
+
+	pinMode(TIMER_LED_OUT, OUTPUT);
+	digitalWrite(TIMER_LED_OUT, LOW);
+
+	startButton.attachPress(startTimer);
+
+	// Ініціалізація таймера (ESP32-S3)
+	// divider = 80: 80 МГц / 80 = 1 МГц (1 tick = 1 мкс)
+	timer = timerBegin(0, 80, true);
+
+	// Прив'язка функції переривання
+	timerAttachInterrupt(timer, &onTimer, true);
+
+	// Ініціалізація Watchdog Timer (WDT)
+	esp_task_wdt_init(WDT_TIMEOUT_S, false); // true - reset
+	esp_task_wdt_add(NULL);
+
+	Serial.println("Press the button to start the timer...");
+}
+
+void loop() {
+	startButton.tick();
+
+	if (timerFired) {
+		timerFired = false;
+		digitalWrite(TIMER_LED_OUT, LOW);
+		const uint32_t elapsedTime = millis() - timeDuration;
+		Serial.printf("Timer finished %u times. Elapsed time: %lu ms\n",
+			isrCounter.load(std::memory_order_relaxed), elapsedTime);
+
+		esp_task_wdt_reset();
 	}
-
-    currectLed.Set(LedState::On);
-         
-	if(currectLed.timer < millis())
-    currectLed.timer =  millis() + delay*2;
-
 }
-
 
