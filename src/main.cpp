ea9ca8a4e8a5cdf105b4017504b32b5ff5173a68
Mykyta <mykyta@marginalact.com>
2026-09-02 17:14:58 +0300

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,71 @@
 #include <Arduino.h>
 
+#include <Arduino.h>
+
 #define PIN_IN 16
 #define PIN_OUT 17
 
-#define ADCres 4095
 bool turnedOn = false;
-float switchTimer = 0;
+uint32_t switchTimer = 0;
 
-#define MinSwitchmV 80.0f
-#define MaxSwitchmV 120.0f
-#define TimderDelay 2.0f
+volatile uint32_t finishTimer = 0;
+volatile bool contactChanged = false;
+
+uint32_t startTimer = 0;
+
+#define DEBOUNCE_TIME 100
+#define TimderDelay 3000
+
+void IRAM_ATTR onInterrupt()
+{
+    static uint32_t lastInterruptTime = 0;
+
+    uint32_t currentTime = millis();
+
+    if (currentTime - lastInterruptTime < DEBOUNCE_TIME)
+        return;
+
+    lastInterruptTime = currentTime;
+
+    finishTimer = currentTime;
+    contactChanged = true;
+}
 
 void setup()
 {
     Serial.begin(115200);
 
-    pinMode(PIN_IN, INPUT_PULLUP);
+    pinMode(PIN_IN, INPUT_PULLDOWN);
     pinMode(PIN_OUT, OUTPUT);
+
+    digitalWrite(PIN_OUT, LOW);
+
+    attachInterrupt(
+        digitalPinToInterrupt(PIN_IN),
+        onInterrupt,
+        RISING);
 }
-void SwitchOnOff(float voltage)
+
+void SwitchOnOff()
 {
-  
     if (switchTimer > millis())
         return;
 
-    if (voltage < MinSwitchmV)
+    if (!turnedOn)
     {
-        turnedOn = true;
+        contactChanged = false;
+        finishTimer = 0;
 
+        startTimer = millis();
+
+        turnedOn = true;
+        digitalWrite(PIN_OUT, HIGH);
     }
-    else if (voltage > MaxSwitchmV)
+    else
     {
         turnedOn = false;
 
+        digitalWrite(PIN_OUT, LOW);
     }
 
     switchTimer = millis() + TimderDelay;
@@ -40,25 +73,16 @@ void SwitchOnOff(float voltage)
 
 void loop()
 {
-    int ADCValue = digitalRead(PIN_IN);
-    int AnalogValue = analogRead(PIN_IN);
-    int AnalogVoltage = analogReadMilliVolts(PIN_IN);
+    SwitchOnOff();
 
-    const float URef = 3100;
+    if (turnedOn && contactChanged)
+    {
+        uint32_t responseTime = finishTimer - startTimer;
 
-    float Voltage = ((float)AnalogValue / (float)ADCres) * URef;
+        Serial.printf("StartTime: %lu\n", startTimer);
+        Serial.printf("FinishTime: %lu\n", finishTimer);
+        Serial.printf("Relay response: %lu ms\n", responseTime);
 
-    SwitchOnOff(Voltage);
-
-    if (turnedOn)
-        digitalWrite(PIN_OUT, HIGH);
-    else
-        digitalWrite(PIN_OUT, LOW);
-
-    Serial.printf("digitalRead: %d \n", ADCValue);
-    Serial.printf("AnalogValue: %d mV\n", AnalogValue);
-    Serial.printf("Voltage: %.02f mV\n", Voltage);
-    Serial.printf("AnalogValueM: %.02f mV\n", AnalogVoltage);
-
-    delay(100);
+        contactChanged = false;
+    }
 }
