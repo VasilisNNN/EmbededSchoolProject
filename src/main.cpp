af3e7517115831ab6ef9ee44f636ae60fe78bf12
Mykyta <mykyta@marginalact.com>
2026-09-11 19:52:31 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,9 @@
 static bool IRAM_ATTR timer_on_alarm_cb(gptimer_handle_t timer,
                                          const gptimer_alarm_event_data_t *edata,
                                          void *user_data) {
-    bool led_state = gpio_get_level(LED_OUT);
-    gpio_set_level(LED_OUT, !led_state);
+    static bool led_state ;
+    led_state = !led_state;
+    gpio_set_level(LED_OUT, led_state);
 
     return true;
 }
