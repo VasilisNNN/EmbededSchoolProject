e9fd6c2a45cdc909daf0853c42e0da306f34d623
Mykyta <mykyta@marginalact.com>
2026-09-18 20:50:04 +0300

    start

--- Src/app/main_app.cpp
+++ Src/app/main_app.cpp
@@ -1,29 +1,49 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "main.h"
-#include "adc/adc.h"
 #include "printf/usb_printf.h"
+#include "pwm/pwm.h"
+#include "adc/adc.h"
 
-#define LED_INBUILT_PIN GPIO_PIN_13
-#define LED_INBUILT_PORT GPIOC
 
-extern "C" void main_cpp() {
-    const uint32_t adc_channels[] = {ADC_CHANNEL_5, ADC_CHANNEL_6};
-    uint32_t adc_values[2];
 
-    if (ADC_Init(adc_channels, 2) != HAL_OK) {
-        Error_Handler();
+extern "C" void main_cpp()
+{
+    PwmDriver_t pwm_led;
+    const uint32_t adcpin = 1;
+    const uint32_t adcref = 4095;
+
+    if (!Pwm_InitByPin(&pwm_led, PWM_PORT_B, 4, 1000, 0))
+    {
+        printf("PWM init failed\n");
     }
 
-    while(1) {
-        HAL_GPIO_WritePin(LED_INBUILT_PORT, LED_INBUILT_PIN, GPIO_PIN_SET);
-	    HAL_Delay(500);
-	    HAL_GPIO_WritePin(LED_INBUILT_PORT, LED_INBUILT_PIN, GPIO_PIN_RESET);
-	    HAL_Delay(500);
+  
+    ADC_Init(&adcpin,1);
 
-        if (ADC_ReadSequence(adc_values, 2) == HAL_OK) {
-            printf("ADC PA5: %lu | ADC PA6: %lu\n",
-                                    adc_values[0], adc_values[1]);
-        }
+    float duty = 0;
+    int32_t step = 10;
+    uint32_t adcvalue = 0;
+    while (1)
+    {
+        /*  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+          HAL_Delay(50);
+          HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+          HAL_Delay(50);
+         */
+
+        ADC_ReadSequence(&adcvalue,1);
+
+
+         duty = ((float)adcvalue / (float)adcref) * 100.0;
+
+        
+        Pwm_SetDutyPercent(&pwm_led, (uint32_t)duty);
+        Pwm_SetDutyPercent(&pwm_led, (uint32_t)duty);
+        printf("PWM duty = %ld%%\n ADC value = %ld\n", (long)duty, adcvalue);
+
+
+
+        HAL_Delay(100);
     }
 }
