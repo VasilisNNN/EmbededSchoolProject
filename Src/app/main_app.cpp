bb1672b8be48baddf1ecfc5c3c3923ccfe3e4480
Mykyta <mykyta@marginalact.com>
2026-09-26 16:56:42 +0300

    final motor

--- Src/app/main_app.cpp
+++ Src/app/main_app.cpp
@@ -10,69 +10,50 @@
 #define SERVO_GPIO_PIN 4
 #define SERVO_STEP_ANGLE 10
 
+extern "C" void main_cpp()
+{
 
-
-extern "C" void main_cpp() {
-
-
-
-  bool error = false;
-  //ADC_HandleTypeDef hadc1;
- // const uint32_t adccount = 1;
-
- /* if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }*/
-
-
-    /*if (!Pwm_InitByPin(&pwm_led, PWM_PORT_B, 4, 1000, 0))
-    {
-        printf("PWM init failed\n");
-    }*/
-
-
+    bool error = false;
+ 
     PwmDriver_t servo_pwm;
     Servo_t servo;
 
     if (!Pwm_InitByPin(&servo_pwm, SERVO_GPIO_PORT,
-                SERVO_GPIO_PIN, SERVO_FREQUENCY_HZ, 0)) {
+                       SERVO_GPIO_PIN, SERVO_FREQUENCY_HZ, 0))
+    {
         error = true;
         printf("PWM init failed\n");
     }
 
-    if (!error && !Servo_Init(&servo, &servo_pwm)) {
+    if (!error && !Servo_Init(&servo, &servo_pwm))
+    {
         error = true;
         printf("Servo init failed\n");
     }
 
-
- 
-
-
-
     const uint32_t adc_channels[] = {ADC_CHANNEL_5};
-     uint32_t adc_values[1];
+    uint32_t adc_values[1];
 
-    if (ADC_Init(adc_channels, 1) != HAL_OK) {
+    if (ADC_Init(adc_channels, 1) != HAL_OK)
+    {
         Error_Handler();
     }
 
-    while (1) {
+    float timer = HAL_GetTick();
+    while (1)
+    {
+        if (ADC_ReadSequence(adc_values, 1) != HAL_OK)
+            continue;
 
- printf("HELLO");
-       
-  if (ADC_ReadSequence(adc_values, 1) == HAL_OK) {
-            printf("ADC PA5: %lu \n",adc_values[0]);
+        uint32_t calculatedAngle = ((adc_values[0] * 180) / ADC_MAX_VALUE)*-1 + 180;
+
+        Servo_SetAngle(&servo, calculatedAngle);
+
+        if (timer < HAL_GetTick())
+        {
+
+            printf("SERVO ANGLE: %d\r\n", calculatedAngle);
+            timer = HAL_GetTick() + 100;
         }
-
-
-        float calculatedAngle = ((float)adc_values[0] / (float)ADC_MAX_VALUE) *180.0;
-
-
-              Servo_SetAngle(&servo, calculatedAngle);
-        
-
-        HAL_Delay(50);
     }
 }
